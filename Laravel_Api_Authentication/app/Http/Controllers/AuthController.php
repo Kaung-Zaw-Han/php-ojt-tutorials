75148664a178c3005f119c240deae2815b5ef833
koekzh <koeshwin9999@gmail.com>
2023-07-06 10:10:15 +0630

    Update

--- Laravel_Api_Authentication/app/Http/Controllers/AuthController.php
+++ Laravel_Api_Authentication/app/Http/Controllers/AuthController.php
@@ -1,22 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-       Validator::make($request->all(), [
-            'name' => 'required|min:4',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
-        ])->validate();
-
         $request['password'] = bcrypt($request->password);
         $user = User::create($request->toArray());
         $token = $user->createToken('token')->plainTextToken;
@@ -28,27 +23,25 @@ class AuthController extends Controller
         return response()->json($result, 200);
     }
 
-    public function login(Request $request){
-       $validate = Validator::make($request->all(), [
-            'email' => 'required',
-            'password' => 'required',
-        ])->validate();
-       $user = User::where('email', $request->email)->first();
-       if (!Hash::check($request->password, $user->password)) {
-        return response(['error' => 'Check Your Password...']);
-    }
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!Hash::check($request->password, $user->password)) {
+            return response(['error' => 'Check Your Password...']);
+        }
 
-         $token = $user->createToken('token')->plainTextToken;
+        $token = $user->createToken('token')->plainTextToken;
         $result = [
             'status' => 'Success',
             'message' => 'Successfully login...',
             'data' => $token,
         ];
-        return response()->json($result,200);
+        return response()->json($result, 200);
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $request->user()->currentAccessToken()->delete();
-        return response()->json(['success'=>'Successfully logout...'],200);
+        return response()->json(['success' => 'Successfully logout...'], 200);
     }
 }
--- Laravel_Api_Authentication/routes/api.php
+++ Laravel_Api_Authentication/routes/api.php
@@ -16,14 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware'=>'auth:sanctum'],function(){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
 
